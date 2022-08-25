ff19f615e16e5c8aa8010ec36bb256596ce49a8f
WarpHero <106863351+WarpHero@users.noreply.github.com>
2022-08-25 22:06:32 +0900

    Week02-1
    vector와 algorithm 이용해서 간단히 sort
    생각보다 간단히 풀린 문제
    
    숫자의 차이를 배분하는 문제로 인식해야지 된다

--- Week02-1/13164.cpp
+++ Week02-1/13164.cpp
@@ -1,60 +1,41 @@
 //HappyKindergarten
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
-
-#define MAX 300000
-
-int Func(int num, int arr[], int N) {
-
-	int diff = 0;
-	int res = 0;
-	for (int i = 1; i<N; i++) {
-		if (diff >= arr[i+1] - arr[i]) {
-			diff = arr[i+1] - arr[i];
-		}
-	}
-}
 
 int main() {
 
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	int N, K;
-	int cnt = 0;
+	vector<int> diff;
 
-	int result = 0;
+	int N, K, res = 0, cnt = 0;
 
 	cin >> N >> K;
 
-	int* children = new int[N];
-	int* diff = new int[K];
+	int tmp;
+	cin >> tmp;
 
-	for (int i = 0; i < N; i++) {
-		cin >> children[i];
-	}
-
-	for (int i = N; i > 0; i--) {
-		if (children[i] - children[i - 1] > 0) {
-			diff[cnt] = children[i] - children[i - 1];
+	for (int i = 1; i < N; i++) {
+		int input;
+		cin >> input;
+		if (input - tmp != 0) {
 			cnt++;
+			diff.push_back(input - tmp);
 		}
+		tmp = input;
 	}
-	for(int i=0)
+
+	sort(diff.begin(), diff.end());
 
 	if (K <= cnt) {
-		int n = cnt - K + 1;
-
-		
-
+		for (int i = 0; i < cnt - K + 1; i++) {
+			res += diff[i];
+		}
 	}
-	else {
-		result = 0;
-	}
-
-
-	delete[] children;
+	cout << res << endl;
 
 	return 0;
 }
-
