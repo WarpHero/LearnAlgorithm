e2d4ebfb66aad34a1424c1da78954c3ca970c406
WarpHero <106863351+WarpHero@users.noreply.github.com>
2022-09-11 22:56:15 +0900

    Create 25305.cpp

---
+++ POJ/25305.cpp
@@ -0,0 +1,17 @@
+//25305
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int N, k;
+	cin >> N >> k;
+	int* arr = new int[N];
+	for (int i = 0; i < N;i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+	cout << arr[N - k];
+	delete[] arr;
+	return 0;
+}
